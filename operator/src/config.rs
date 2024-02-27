e41ccedb92ed043135dfebd1357eec5cc913ebbb
paulobressan <paulo.bressan@outlook.com>
2024-02-27 19:31:40 -0300

    chore(operator): removed node port env

--- operator/src/config.rs
+++ operator/src/config.rs
@@ -13,7 +13,6 @@ pub fn get_config() -> &'static Config {
 pub struct Config {
     pub dns_zone: String,
     pub extension_name: String,
-    pub node_port: u16,
     pub api_key_salt: String,
 }
 
@@ -22,9 +21,6 @@ impl Config {
         Self {
             dns_zone: env::var("DNS_ZONE").unwrap_or("demeter.run".into()),
             extension_name: env::var("EXTENSION_NAME").unwrap_or("node-m1".into()),
-            node_port: env::var("NODE_PORT")
-                .map(|e| e.parse().expect("NODE_PORT must be a number u16"))
-                .unwrap_or(9443),
             api_key_salt: env::var("API_KEY_SALT").unwrap_or("cardano-node-salt".into()),
         }
     }
