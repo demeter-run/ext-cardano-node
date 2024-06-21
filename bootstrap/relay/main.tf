b9d40b6ab3c2b237ec20a8c090da3455ec53b0be
Chris Gianelloni <wolf31o2@blinklabs.io>
2024-06-21 11:02:55 -0400

    feat: gcp cloud provider
    Support GCP as a cloud provider.
    
    To ensure backwards compatibility, the default is configured for AWS.
    
    Signed-off-by: Chris Gianelloni <wolf31o2@blinklabs.io>

--- bootstrap/relay/main.tf
+++ bootstrap/relay/main.tf
@@ -2,7 +2,13 @@ variable "namespace" {
   description = "the namespace where the resources will be created"
 }
 
-resource "kubernetes_service_v1" "node-relay-n2n" {
+variable "cloud_provider" {
+  type    = string
+  default = "aws"
+}
+
+resource "kubernetes_service_v1" "node-relay-n2n-aws" {
+  for_each = toset([for n in toset(["loadbalancer"]) : n if var.cloud_provider == "aws"])
   metadata {
     name      = "node-relay-n2n"
     namespace = var.namespace
@@ -16,6 +22,47 @@ resource "kubernetes_service_v1" "node-relay-n2n" {
   spec {
     type                = "LoadBalancer"
     load_balancer_class = "service.k8s.aws/nlb"
+
+    selector = {
+      "role"    = "node"
+    }
+
+    port {
+      name        = "mainnet"
+      protocol    = "TCP"
+      port        = 3000
+      target_port = "n2n-mainnet"
+    }
+
+    port {
+      name        = "preprod"
+      protocol    = "TCP"
+      port        = 3001
+      target_port = "n2n-preprod"
+    }
+
+    port {
+      name        = "preview"
+      protocol    = "TCP"
+      port        = 3002
+      target_port = "n2n-preview"
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "node-relay-n2n-gcp" {
+  for_each = toset([for n in toset(["loadbalancer"]) : n if var.cloud_provider == "gcp"])
+  metadata {
+    name      = "node-relay-n2n"
+    namespace = var.namespace
+    annotations = {
+      "cloud.google.com/l4-rbs" : "enabled"
+    }
+  }
+
+  spec {
+    type                    = "LoadBalancer"
+    external_traffic_policy = "Local"
 
     selector = {
       "role"    = "node"
